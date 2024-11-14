3c013df0c01634de52e60731f4e45640a027388c
root <root@ninesun.novastar.com>
2024-11-14 11:00:15 +0800

    update role

--- scripts/update-role.sh
+++ scripts/update-role.sh
@@ -5,7 +5,7 @@ FUNCTION_NAME="rds-backup-to-oss"
 ROLE_NAME="rds-backup-to-oss-role"
 AWS_REGION="ap-southeast-2"
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-PROJECT_ROOT="$( cd "$SCRIPT_DIR/.." && pwd )"
+PROJECT_ROOT="$( cd "$SCRIPT_DIR" && pwd )"
 
 # 颜色输出
 RED='\033[0;31m'
